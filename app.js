7e89f8078cae88fae35f8f9fe90aa25deb3a7087
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-10-24 23:23:47 +0500

    ToDo:Added Edit functionality

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 var input
 var list
+var value
 function getToDo() {
-     input = document.getElementById("input");
+    input = document.getElementById("input");
 
-    var value = input.value
+    value = input.value
 
     list = document.getElementById("list")
-    list.innerHTML += `<li > ${value} <button id = "delete" onclick = "removeToDo(event)">Delete</button></li> ` 
+    list.innerHTML += `<li > <input  value = ${value} disabled  > <button id = "delete" onclick = "removeToDo(event)">Delete</button><button onclick="editToDo(event)"> Edit </button></li>  `
 
     input.value = ""
 
 }
 
-function enterToDo(event){
-    if(event.keyCode === 13){
+function editToDo(event) {
+    var edit = event.target.parentNode.childNodes[1]
+    edit.disabled = false
+    edit.focus()
+    event.target.innerHTML = "Update"
+
+    event.target.setAttribute("onclick", "updateToDo(event)")
+
+}
+
+function updateToDo(event) {
+    var edit = event.target.parentNode.childNodes[1];
+    edit.disabled = true
+    var updatedValue = edit.value
+
+    
+    event.target.innerHTML = "Edit"
+    
+    event.target.setAttribute("onclick", "editToDo(event)")
+    
+    return updatedValue
+}
+
+
+
+
+
+
+
+
+function enterToDo(event) {
+    if (event.keyCode === 13) {
         getToDo()
     }
 
@@ -25,10 +56,10 @@ function enterToDo(event){
 
 
 
-function removeToDo(event){
-    
+function removeToDo(event) {
+
     event.target.parentNode.remove()
-    
-    
+
+
 
 }
