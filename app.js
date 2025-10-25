8a4f8fc9d65699ea2351f6fb0f2f1ca216b14f33
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-10-25 22:12:09 +0500

    ToDo:Pending tasks functionality added

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 var input
 var list
 var value
+var total = 0;
 function getToDo() {
-    
+
     input = document.getElementById("input");
 
     value = input.value.trim()
 
 
-    if(value === ""){
+    if (value === "") {
         alert("empty")
         return
     }
 
-    
+
 
     list = document.getElementById("list")
     list.innerHTML += `<li ><input onclick = "checkedToDo(event)" type = 
     "checkbox"> <input  value = ${value} disabled  > <button id = "delete" onclick = "removeToDo(event)">Delete</button><button onclick="editToDo(event)"> Edit </button></li>  `
+    total++
+    console.log(total, "total from get")
+    pendingCounter()
 
     input.value = ""
 
 }
 
 function editToDo(event) {
-    
+
     var edit = event.target.parentNode.childNodes[2]
     edit.disabled = false
-    edit.setAttribute("class","editInput")
+    edit.setAttribute("class", "editInput")
     edit.focus()
     event.target.innerHTML = "Update"
 
@@ -39,35 +43,39 @@ function editToDo(event) {
 function updateToDo(event) {
     var edit = event.target.parentNode.childNodes[2];
     var updatedValue = edit.value
-    edit.setAttribute("class","")
+    edit.setAttribute("class", "")
 
     edit.disabled = true
-    
+
     event.target.innerHTML = "Edit"
-    
+
     event.target.setAttribute("onclick", "editToDo(event)")
 
-    edit.setAttribute("value",updatedValue)
+    edit.setAttribute("value", updatedValue)
 
     return updatedValue
 }
 
 
-function deleteAllToDo(){
+function deleteAllToDo() {
     list.innerHTML = ""
 }
 
 
-function checkedToDo(event){
+function checkedToDo(event) {
     var inputLine;
-    if(event.target.checked === true){
+    if (event.target.checked === true) {
+        event.target.disabled = true
         inputLine = event.target.parentNode.childNodes[2]
-        inputLine.setAttribute("class","linethough")
+
+        inputLine.setAttribute("class", "linethough")
         console.log("completed")
         completeCounter()
+        var pendingE = document.getElementById("pendingC");
+        pendingE.childNodes[3].innerHTML = `<h3>${--pending}</h3>`
+
 
     }
-    console.log("Checked")
 }
 
 
@@ -83,18 +91,45 @@ function enterToDo(event) {
 
 
 
-
+var status = "normal"
 function removeToDo(event) {
 
     event.target.parentNode.remove()
-
+    --total
+    console.log(total, "total from remove")
+    if (pending > 0) {
+        pendingCounter()
+    }
+    --count
+    status = "remove"
+    completeCounter()
 
 }
 
 
 var count = 0
-function completeCounter(){
-    var complete = document.getElementById("complete");
-    count++
-    complete.childNodes[3].innerHTML = `<h3>${count}</h3>`
+function completeCounter() {
+    if (status === "remove") {
+        console.log("if running")
+        var complete = document.getElementById("complete");
+        complete.childNodes[3].innerHTML = `<h3>${count}</h3>`
+    } else {
+        console.log("else")
+        var complete = document.getElementById("complete");
+        count++
+        complete.childNodes[3].innerHTML = `<h3>${count}</h3>`
+    }
+}
+
+var pending;
+function pendingCounter() {
+    var pendingE = document.getElementById("pendingC");
+    pending = total - count;
+
+    pendingE.childNodes[3].innerHTML = `<h3>${pending}</h3>`
+
+
+    console.log(total, "total from pending")
+    console.log(pending, "pending")
+    pendingE.childNodes[3].innerHTML = `<h3>${pending}</h3>`
 }
